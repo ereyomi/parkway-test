fa1adb09aa1b93436f4c527c567f02f0494a82a6
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-10-16 10:34:38 +0100

    refactor: warnTheDev function

--- js/main.js
+++ js/main.js
@@ -2,13 +2,8 @@
 const warnTheDeveoper = ( devNumOfChair, numOfSweet, chairStartPosition ) => {
     let warnDev = chairStartPosition
     for (let sweetIndex = 1; sweetIndex < numOfSweet; sweetIndex++) {
-        if (warnDev >= devNumOfChair) {
-            // reset warn
-            warnDev = 1
-        } else
-        {
-            warnDev++
-        }
+        // check and continue process or possibly reset warnDev to 1
+        ( warnDev >= devNumOfChair ) ? warnDev = 1 : warnDev++
         
     }
     console.log('Warn the developer siting on sit', warnDev)
